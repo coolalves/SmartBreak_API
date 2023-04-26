ca3f2eefbdc35f3ea43fea7b8f89a7770fd8f29a
Daniel Alves <alvesdaniel@ua.pt>
2023-04-26 18:35:38 +0100

    auth feito so falta jwt

--- routes/auth.js
+++ routes/auth.js
@@ -1,35 +1,60 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/usersModel");
+const bcrypt = require("bcrypt");
 
 //Registar user
 router.post("/register", async (req, res) => {
+  const userExists = await User.findOne({ email: req.body.email });
+  const salt = await bcrypt.genSalt(12);
+  const passwordHash = await bcrypt.hash(req.body.password, salt);
+
   const user = new User({
     name: req.body.name,
     surname: req.body.surname,
     email: req.body.email,
-    password: req.body.password,
+    password: passwordHash,
     admin: req.body.admin,
     department: req.body.department,
   });
 
   if (
-    !user.name ||
-    !user.surname ||
-    !user.email ||
-    !user.password ||
-    !user.admin ||
-    !user.department
+    !req.body.name ||
+    !req.body.surname ||
+    !req.body.email ||
+    !req.body.password ||
+    !req.body.admin ||
+    !req.body.department
   ) {
     return res.status(400).json({ message: "Please fill all the fields" });
+  } else if (!userExists) {
+    try {
+      const newUser = await user.save();
+      res.status(201).json(newUser);
+    } catch (err) {
+      res.status(400).json({ message: err.message });
+    }
+  } else {
+    return res.status(400).json({ message: "User already exists" });
+  }
+});
+
+//login user
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    return res.status(400).json({ message: "Invalid email or password" });
   }
 
-  try {
-    const newUser = await user.save();
-    res.status(201).json(newUser);
-  } catch (err) {
-    res.status(400).json({ message: err.message });
+  const isMatch = await bcrypt.compare(password, user.password);
+
+  if (!isMatch) {
+    return res.status(400).json({ message: "Invalid email or password" });
   }
+
+  res.status(200).json({ message: "Logged in successfully" });
 });
 
 module.exports = router;

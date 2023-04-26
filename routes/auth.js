ecaacad9d2a6c09e985b6501b3413814cff7a66f
Daniel Alves <alvesdaniel@ua.pt>
2023-04-26 19:14:45 +0100

    auth quase pronta, falta rever a ultima função do auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/usersModel");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 //Registar user
 router.post("/register", async (req, res) => {
@@ -54,7 +55,61 @@ router.post("/login", async (req, res) => {
     return res.status(400).json({ message: "Invalid email or password" });
   }
 
-  res.status(200).json({ message: "Logged in successfully" });
+  //try catch
+  try {
+    const secret = process.env.JWT_SECRET;
+    const token = jwt.sign(
+      {
+        id: user._id,
+      },
+      secret
+    );
+
+    res.status(200).json({ message: "Logged in successfully", token });
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({
+      message: "Something went wrong",
+    });
+  }
+});
+
+//verificar token
+function verifyToken(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({ message: "You are not authenticated" });
+  }
+
+  try {
+    const secret = process.env.JWT_SECRET;
+    jwt.verify(token, secret);
+
+    next();
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({
+      message: "Invalid Token",
+    });
+  }
+}
+
+//private route para ir buscar um user por id
+//DEPOIS IMPLEMENTAR ESTA VERSÃO
+router.get("/users/:id", verifyToken, async (req, res) => {
+  const id = req.params.id;
+
+  //check if user exists
+  const user = await User.findById(id, "-password");
+  if (!user) {
+    return res.status(404).json({ message: "User not found" });
+  } else {
+    res.status(200).json(user);
+  }
+
+  res.status(200).json({ user });
 });
 
 module.exports = router;

863af5c61598e54b223a45079df827ba3d4618d4
Diana Siso <diana.siso@ua.pt>
2023-05-03 11:44:16 +0100

    .

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const mongoose = require("mongoose")
 mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true})
     .then(() => {
         console.log('Connected to Database')
-        app.listen(process.env.PORT, () => {
+        app.listen((process.env.PORT || 4000), () => {
             console.log("Server Started")
         })
     })

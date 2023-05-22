0cd4b0d6f9f0c2a12ea68aa6e09dc7c7a1e5d7da
Diana Siso <diana.siso@ua.pt>
2023-05-22 17:14:14 +0100

    .

--- routes/users.js
+++ routes/users.js
@@ -8,12 +8,12 @@ router.get("/", verifyToken, async (req, res) => {
   try {
     const authHeader = req.headers["authorization"];
     const token = authHeader && authHeader.split(" ")[1];
-    const user = await User.find({token : token})
+    const user = await User.find({ token: token })
     if (!user[0].access)
       return res.status(403).json({ message: "Cannot access the content" });
 
     const users = await User.find();
-    res.status(200).json({message: users});
+    res.status(200).json({ message: users });
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
@@ -24,72 +24,75 @@ router.get("/:id", verifyToken, async (req, res) => {
   try {
     const authHeader = req.headers["authorization"];
     const token = authHeader && authHeader.split(" ")[1];
-    const user = await User.find({token : token})
+    const user = await User.find({ token: token })
     if (user[0].id != req.params.id)
       return res.status(403).json({ message: "Cannot access the content" });
 
-    res.status(200).json({message: user});
+    res.status(200).json({ message: user });
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
 });
 
 //UPDATE ONE USER
-router.patch("/:id", verifyToken, getUser, async (req, res) => {
-  let user;
+router.patch("/:id", verifyToken, async (req, res) => {
   try {
-    user = await User.findById(req.params.id);
-    if (user == null) {
-      return res.status(404).json({ message: "Cannot find user" });
+    let user;
+    const authHeader = req.headers["authorization"];
+    const token = authHeader && authHeader.split(" ")[1];
+    user = await User.find({ token: token })
+    if (user[0].id != req.params.id)
+      return res.status(403).json({ message: "Cannot access the content" });
+
+    res.user = user;
+    if (req.body.name != null) {
+      res.user.name = req.body.name;
     }
-  } catch (err) {
-    return res.status(500).json({ message: err.message });
+    if (req.body.surname != null) {
+      res.user.surname = req.body.surname;
+    }
+    if (req.body.email != null) {
+      res.user.email = req.body.email;
+    }
+    if (req.body.password != null) {
+      res.user.password = req.body.password;
+    }
+    if (req.body.admin != null) {
+      res.user.admin = req.body.admin;
+    }
+    if (req.body.battery != null) {
+      res.user.battery = req.body.battery;
+    }
+    if (req.body.department != null) {
+      res.user.department = req.body.department;
+    }
+    if (req.body.total_battery != null) {
+      res.user.total_battery = req.body.total_battery;
+    }
+    if (req.body.pause != null) {
+      res.user.pause = req.body.pause;
+    }
+    if (req.body.rewards != null) {
+      res.user.rewards = req.body.rewards;
+    }
+    if (req.body.accessibility != null) {
+      res.user.accessibility = req.body.accessibility;
+    }
+    if (req.body.notifications != null) {
+      res.user.notifications = req.body.notifications;
+    }
+    if (req.body.permissions != null) {
+      res.user.permissions = req.body.permissions;
+    }
+    try {
+      const updatedUser = await res.user.save();
+      res.status(200).json({ message: updatedUser });
+    } catch (err) {
+      res.status(400).json({ message: err.message });
+    }
   }
-  res.user = user;
-  if (req.body.name != null) {
-    res.user.name = req.body.name;
-  }
-  if (req.body.surname != null) {
-    res.user.surname = req.body.surname;
-  }
-  if (req.body.email != null) {
-    res.user.email = req.body.email;
-  }
-  if (req.body.password != null) {
-    res.user.password = req.body.password;
-  }
-  if (req.body.admin != null) {
-    res.user.admin = req.body.admin;
-  }
-  if (req.body.battery != null) {
-    res.user.battery = req.body.battery;
-  }
-  if (req.body.department != null) {
-    res.user.department = req.body.department;
-  }
-  if (req.body.total_battery != null) {
-    res.user.total_battery = req.body.total_battery;
-  }
-  if (req.body.pause != null) {
-    res.user.pauses = req.body.pauses;
-  }
-  if (req.body.rewards != null) {
-    res.user.rewards = req.body.rewards;
-  }
-  if (req.body.accessibility != null) {
-    res.user.accessibility = req.body.accessibility;
-  }
-  if (req.body.notifications != null) {
-    res.user.notifications = req.body.notifications;
-  }
-  if (req.body.permissions != null) {
-    res.user.permissions = req.body.permissions;
-  }
-  try {
-    const updatedUser = await res.user.save();
-    res.status(200).json({message: updatedUser});
-  } catch (err) {
-    res.status(400).json({ message: err.message });
+  catch (err) {
+    res.status(500).json({ message: err.message });
   }
 });
 
@@ -110,7 +113,7 @@ router.get("/department/:id", verifyToken, async (req, res) => {
     if (users == null) {
       return res.status(404).json({ message: "Cannot find users" });
     }
-    res.status(200).json({message: users});
+    res.status(200).json({ message: users });
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
@@ -127,29 +130,14 @@ router.get("/department/:id/page/:page", verifyToken, async (req, res) => {
     if (isNaN(req.params.page))
       return res.status(400).json({ message: req.params.page + " is not a number" });
 
-    const totalPages = Math.floor(users.length/10) + 1 
+    const totalPages = Math.floor(users.length / 10) + 1
 
-    res.status(200).json({total_pages: totalPages, results: users.slice((req.params.page - 1) *10, req.params.page*10)});
+    res.status(200).json({ total_pages: totalPages, results: users.slice((req.params.page - 1) * 10, req.params.page * 10) });
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
 });
 
-
-
-async function getUser(req, res, next) {
-  let user;
-  try {
-    user = await User.findById(req.params.id);
-    if (user == null) {
-      return res.status(404).json({ message: "Cannot find user" });
-    }
-  } catch (err) {
-    return res.status(500).json({ message: err.message });
-  }
-  res.user = user;
-  next();
-}
 
 
 module.exports = router;

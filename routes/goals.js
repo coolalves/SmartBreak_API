ca513430fed6e78c9d37af6a07789dc80c189b3b
Diana Siso <diana.siso@ua.pt>
2023-05-23 17:34:09 +0100

    reestruturação dos goals

--- routes/goals.js
+++ routes/goals.js
@@ -125,9 +125,8 @@ router.get("/destination/:id/inactive", verifyToken, async (req, res) => {
     const authHeader = req.headers["authorization"];
     const token = authHeader && authHeader.split(" ")[1];
     const user = await User.findOne({ token: token })
-
-    if (!user.department != req.params.id)
-      return res.status(403).json({ DEP_ID: req.params.id, DEP_USER: user.department, message: "Cannot access the content" });
+    if (user.id != req.params.id && user.department != req.params.id)
+      return res.status(403).json({ message: "Cannot access the content" });
 
     let goals = [];
     const elements = await Goal.find({ organization: user.organization });

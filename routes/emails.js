19daa976fbfb307caf3f7c798a9f88979671482b
Diana Siso <diana.siso@ua.pt>
2023-06-14 16:51:00 +0100

    email

--- routes/emails.js
+++ routes/emails.js
@@ -7,37 +7,37 @@ const nodemailer = require("nodemailer");
 //GET ALL EMAILS
 router.get("/", async (req, res) => {
     try {
-      const users = await User.find();
-      const userData = users.map(user => ({ email: user.email, date_created: user.created }));
-      res.status(200).json({ message: userData, total: users.length });
+        const users = await User.find();
+        const userData = users.map(user => ({ email: user.email, date_created: user.created }));
+        res.status(200).json({ message: userData, total: users.length });
     } catch (err) {
-      res.status(500).json({ message: err.message });
+        res.status(500).json({ message: err.message });
     }
-  });
+});
 
 router.post("/recover", async (req, res) => {
-        const transporter = nodemailer.createTransport({
-            "service" : "gmail",
-            "auth" : {
-                "user" : process.env.MAIL,
-                "pass" : process.env.PASS,
-            }
-        })
-
-        const mailBody = {
-            "from" : process.env.MAIL,
-            "to" : req.body.email,
-            "subject" : "Recuperação de palavra-passe",
-            "text" : "A tua nova palavra-passe é " + req.body.pass + ". Faz login na aplicação para alterares a informação."
+    const transporter = nodemailer.createTransport({
+        "service": "gmail",
+        "auth": {
+            "user": process.env.MAIL,
+            "pass": process.env.PASS,
         }
-        try {
-            await transporter.sendMail(mailBody);
-            res.status(200).json({ message: 'Email enviado com sucesso.' });
-          } catch (error) {
-            res.status(500).json({ message: error });
-          }
+    })
 
-    
+    const mailBody = {
+        "from": process.env.MAIL,
+        "to": req.body.email,
+        "subject": "Recuperação de palavra-passe",
+        "text": "A tua nova palavra-passe é " + req.body.pass + ". Faz login na aplicação para alterares a informação."
+    }
+    try {
+        await transporter.sendMail(mailBody);
+        res.status(200).json({ message: 'Email enviado com sucesso.' });
+    } catch (error) {
+        res.status(500).json({ message: 'erro' + error });
+    }
+
+
 })
 
 module.exports = router;
